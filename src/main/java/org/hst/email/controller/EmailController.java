d8941c83d41eacd84940451aa26b64143e5c0001
Paulo Ferreira <phgferreira@outlook.com>
2021-03-18 09:51:03 -0300

    Modificado temporariamente para não devolver nenhuma resposta

--- src/main/java/org/hst/email/controller/EmailController.java
+++ src/main/java/org/hst/email/controller/EmailController.java
@@ -1,5 +1,8 @@
 package org.hst.email.controller;
 
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,8 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class EmailController {
 
 	@PostMapping("/send")
-	private ResponseEntity<?> sendEmail(@RequestBody String content) {
-		System.out.println("Sucess: " + content);
-		return ResponseEntity.ok(content);
+	private void sendEmail(@RequestBody Map<String, String> body) {
+		System.out.println("Sucess: " + body.toString());
+		
+		//return ResponseEntity.status(HttpStatus.OK).body("Success");
 	}
 }

162e0c10ca3ca15d01bdbdcc17a76475c93148e3
Paulo Ferreira <phgferreira@outlook.com>
2021-03-18 11:23:26 -0300

    Configurado o disparo de e-mail

--- src/main/java/org/hst/email/controller/EmailController.java
+++ src/main/java/org/hst/email/controller/EmailController.java
@@ -1,9 +1,16 @@
 package org.hst.email.controller;
 
 import java.util.Map;
+import java.util.Properties;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,7 +24,35 @@ public class EmailController {
 
 	@PostMapping("/send")
 	private void sendEmail(@RequestBody Map<String, String> body) {
-		System.out.println("Sucess: " + body.toString());
+		// Propriedades do envio de e-mail (TLS Protocol)
+		Properties props = System.getProperties();
+		props.setProperty("mail.smtp.host", "smtp.office365.com");
+		props.setProperty("mail.smtp.port", "587");
+		props.setProperty("mail.smtp.auth", "true");
+		props.setProperty("mail.smtp.starttls.enable", "true");
+
+		// Autenticação de e-mail
+		Authenticator auth = new Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication("paulo.ferreira@hst.org.br", "l!f3Line8");
+			}
+		};
+
+		try {
+			// Consolidação e preparo do e-mail
+			Session sessao = Session.getInstance(props, auth);
+			MimeMessage msg = new MimeMessage(sessao);
+			msg.setFrom(new InternetAddress("paulo.ferreira@hst.org.br"));
+			msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(body.get("recipient")));
+			//msg.setRecipients(Message.RecipientType.BCC, InternetAddress.parse("paulo.ferreira@hst.org.br"));
+			msg.setSubject(body.get("title"));
+			msg.setText(body.get("content"), "utf-8", "html");
+
+			// Envia e-mail
+			Transport.send(msg);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		
 		//return ResponseEntity.status(HttpStatus.OK).body("Success");
 	}
